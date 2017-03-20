9b3941c26a0dc444d9aaadda828e74dfbcb0e59b
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-20 13:03:46 +0100

    Edited get all emojis

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -36,7 +36,8 @@ class Ihlebot:
     async def emojis(self, ctx):
         """Returns a list of all Server Emojis"""
         for ej in self.bot.get_all_emojis():
-            await self.bot.say(ej.name, ej.id, ej.managed, ej.server)
+            msg = print(ej.name, ej.id, ej.managed, ej.server)
+            await self.bot.say(msg)
 
 def setup(bot):
     n = Ihlebot(bot)
