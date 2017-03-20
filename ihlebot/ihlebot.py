950cdb8e403d76a876fdbb1a7ca8c13807d8f2fb
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-20 12:41:17 +0100

    Forgot an await

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -30,7 +30,7 @@ class Ihlebot:
     async def beleidige(self, ctx, name):
         """Second Test, Variablenverarbeitung"""
         msg = await self.bot.say(name + ' ist ein Behindi!')
-        self.bot.add_reaction(msg,'👀')
+        await self.bot.add_reaction(msg,'👀')
 
 #@client.event
 #async def add_reaction(message):
