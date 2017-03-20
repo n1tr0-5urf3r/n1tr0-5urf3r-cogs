2e6093bc235667e3b1793fb809bb042c38a8035e
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-20 15:40:16 +0100

    Added ping command

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -58,6 +58,8 @@ class Ihlebot:
         start = time.time()
         response = os.system("sudo ping -c 1 -w3 " + ip)
         duration = time.time()-start
+        await self.bot.say(type(ip))
+
 
         if response == 0:
             await self.bot.say(ip + ' is up and responding in ' + duration + 'ms.')
