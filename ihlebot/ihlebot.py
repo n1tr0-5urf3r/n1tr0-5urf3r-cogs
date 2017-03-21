9a8f0c9dcb0714bb502ba1e03139f99b9aff453a
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-21 09:11:53 +0100

    int

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -101,7 +101,7 @@ class Ihlebot:
                 status = ret.status_code
                 #status = re.sub('^.* ', '', status)
                 await self.bot.say('DEBUG Statuscode: ' + status)
-                if status != '404':
+                if status != 404:
                     valid = True
                     # Download page from static pr0gramm, save to tempfile
                     urllib.request.urlretrieve('http://pr0gramm.com/static/' + post, 'temp.html')
