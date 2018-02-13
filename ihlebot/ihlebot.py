9b1e9954c2479c53482140db1f70a36f5a966d55
fabian.ihle <fabian.ihle@hfwu.de>
2018-02-13 13:42:56 +0100

    debugging

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -161,7 +161,7 @@ class Ihlebot:
                 os.remove('temp.html')
 
     @commands.command(pass_context=True)
-    async def coinflip(self, ctx, player1=None, *, player2):
+    async def coinflip(self, ctx, player1=None, *, player2=None):
         """Coinflip, defaults to Kopf/Zahl if no players are given"""
         rng = randint(1,10)
 
