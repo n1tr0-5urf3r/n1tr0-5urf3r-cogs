169da58fb4719616604ab868e364a98826687f72
fabi@ihlecloud.de <soko5113>
2018-04-30 14:49:21 +0200

    Initial commit for !gruppeverlassen

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -34,9 +34,11 @@ class Ihlebot:
     @commands.group(pass_context=True)
     async def ihle(self, ctx):
         """First Test, Commandcall"""
-        await self.bot.say('Ihle ist der beste!')
+        await
+        self.bot.say('Ihle ist der beste!')
         game = discord.Game(name='Justified Loyalty')
-        await self.bot.change_status(game)
+        await
+        self.bot.change_status(game)
 
     # @commands.command(pass_context=True)
     # async def beleidige(self, ctx, name):
@@ -60,18 +62,21 @@ class Ihlebot:
         ]
 
         rng = random.randint(0, len(pizza_list))
-        await self.bot.say(pizza_list[rng])
+        await
+        self.bot.say(pizza_list[rng])
 
     @commands.command(pass_context=True)
     async def emojis(self, ctx):
         """Returns a list of all Server Emojis"""
         server = ctx.message.server
-        await self.bot.say('This may take some time, generating list...')
+        await
+        self.bot.say('This may take some time, generating list...')
         data = discord.Embed(description="Emojilist")
         for ej in server.emojis:
             data.add_field(
                 name=ej.name, value=str(ej) + " " + ej.id, inline=False)
-        await self.bot.say(embed=data)
+        await
+        self.bot.say(embed=data)
 
     # @commands.command(pass_context=True)
     # async def create(self, ctx):
@@ -140,7 +145,8 @@ class Ihlebot:
         data.set_thumbnail(
             url='https://cdn.discordapp.com/emojis/294742647069868032.png')
 
-        await self.bot.say(embed=data)
+        await
+        self.bot.say(embed=data)
 
     @commands.command(pass_context=True)
     async def ping(self, ctx, ip):
@@ -156,7 +162,8 @@ class Ihlebot:
         elif valid_hostname.match(ip):
             valid = True
             try:
-                await self.bot.say('Doing DNS lookup...')
+                await
+                self.bot.say('Doing DNS lookup...')
                 ip = socket.gethostbyname(ip)
 
                 if valid == True:
@@ -165,15 +172,19 @@ class Ihlebot:
                     duration = time.time() - start
                     duration = round(duration * 1000, 0)
                     if response == 0:
-                        await self.bot.say(ip + ' is up and responding in ' +
-                                           str(duration) + 'ms.')
+                        await
+                        self.bot.say(ip + ' is up and responding in ' +
+                                     str(duration) + 'ms.')
                     else:
-                        await self.bot.say(ip + ' is not reachable.')
+                        await
+                        self.bot.say(ip + ' is not reachable.')
                 else:
-                    await self.bot.say(ip + ' is not a valid IP or Domain.')
+                    await
+                    self.bot.say(ip + ' is not a valid IP or Domain.')
 
             except socket.gaierror:
-                await self.bot.say('Whoops! That Address cant be resolved!')
+                await
+                self.bot.say('Whoops! That Address cant be resolved!')
 
     @commands.command(pass_context=True)
     async def pr0(self, ctx):
@@ -202,8 +213,10 @@ class Ihlebot:
             name="Score",
             value="{0} :arrow_up: {1} :arrow_down:".format(upvotes, downvotes))
 
-        await self.bot.say(embed=embed)
-        await self.bot.say("https://img.pr0gramm.com/{}".format(item))
+        await
+        self.bot.say(embed=embed)
+        await
+        self.bot.say("https://img.pr0gramm.com/{}".format(item))
 
     @commands.command(pass_context=True)
     async def coinflip(self, ctx, player1=None, *, player2=None):
@@ -212,14 +225,18 @@ class Ihlebot:
 
         if player1 is None and player2 is None:
             if rng < 5:
-                return await self.bot.say("Kopf gewinnt!")
+                return await
+                self.bot.say("Kopf gewinnt!")
             else:
-                return await self.bot.say("Zahl gewinnt!")
+                return await
+                self.bot.say("Zahl gewinnt!")
         else:
             if rng < 5:
-                return await self.bot.say("{} hat gewonnen!".format(player1))
+                return await
+                self.bot.say("{} hat gewonnen!".format(player1))
             else:
-                return await self.bot.say("{} hat gewonnen!".format(player2))
+                return await
+                self.bot.say("{} hat gewonnen!".format(player2))
 
     def getColor(self, user):
         try:
@@ -239,16 +256,17 @@ class Ihlebot:
 
         # Probably should make this in a subcommand
         weekday = datetime.datetime.today().weekday()
-        week_start = today - datetime.timedelta(days = weekday)
-        week_end = today + datetime.timedelta(days = 4 - weekday)
+        week_start = today - datetime.timedelta(days=weekday)
+        week_end = today + datetime.timedelta(days=4 - weekday)
         if subcommand:
             if subcommand.lower() == "nextweek" or subcommand.lower() == "nw":
                 cal_week = int(cal_week) + 1
                 weekday = 0
-                week_start = today + datetime.timedelta(days = (7 - today.weekday()))
-                week_end = week_start + datetime.timedelta(days = 4)
+                week_start = today + datetime.timedelta(days=(7 - today.weekday()))
+                week_end = week_start + datetime.timedelta(days=4)
             elif subcommand.lower() == "help" or subcommand.lower() == "h":
-                return await self.bot.say("""```
+                return await
+                self.bot.say("""```
 Mensa:
     help         Diese Nachricht
     <leer>       Speiseplan der aktuellen Woche
@@ -279,14 +297,16 @@ Mensa:
                 t_menu = re.sub("(<.*?>)", "", m[0])
                 t_menu = re.sub("  |, ", "\n- ", t_menu)
                 t_menu = re.sub("Tagessuppe ", "Tagessuppe\n- ", t_menu)
-                t_menu = re.sub("Tagesmenü vegetarisch|Tagesmenü|mensaVital vegan|mensaVital vegetarisch|mensaVital|Angebot des Tages", "", t_menu)
+                t_menu = re.sub(
+                    "Tagesmenü vegetarisch|Tagesmenü|mensaVital vegan|mensaVital vegetarisch|mensaVital|Angebot des Tages",
+                    "", t_menu)
                 daily_menu.append((t_menu))
             return daily_menu
 
         menu1 = cleanUp(tagesmenu)
         menu2 = cleanUp(tagesmenu_veg)
         menu3 = cleanUp(mensa_vital)
-        menu4= cleanUp(tages_angebot)
+        menu4 = cleanUp(tages_angebot)
 
         embed = discord.Embed(
             description="Mensa Morgenstelle, KW {} vom {} bis {}".format(cal_week, week_start.strftime("%d.%m."),
@@ -318,8 +338,8 @@ Mensa:
         embed.set_thumbnail(
             url='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Studentenwerk_T%C3%BCbingen-Hohenheim_logo.svg/220px-Studentenwerk_T%C3%BCbingen-Hohenheim_logo.svg.png')
         embed.set_footer(text='Bot by Fabi')
-        await self.bot.say(embed=embed)
-
+        await
+        self.bot.say(embed=embed)
 
     @commands.command(pass_context=True)
     async def createroles(self, ctx):
@@ -350,23 +370,30 @@ Mensa:
         # Create a role for each channel
         for group_channel in group_channels:
             if group_channel not in all_roles:
-                await self.bot.create_role(author.server, name=group_channel)
-                await self.bot.say("Role {} created".format(group_channel))
+                await
+                self.bot.create_role(author.server, name=group_channel)
+                await
+                self.bot.say("Role {} created".format(group_channel))
 
         # Grant permissions to role
         for channel in all_channels:
             if "übungsgruppe-" in channel.name:
                 role = discord.utils.get(server.roles, name=channel.name)
                 # Deny permission to everyone
-                await self.bot.edit_channel_permissions(channel, server.default_role, everyone_perms)
+                await
+                self.bot.edit_channel_permissions(channel, server.default_role, everyone_perms)
                 # Grant permission to role
-                await self.bot.edit_channel_permissions(channel, role, overwrite)
-                await self.bot.say("Granted permissions for role {} to channel {}".format(role, channel))
-                await asyncio.sleep(1.5)
+                await
+                self.bot.edit_channel_permissions(channel, role, overwrite)
+                await
+                self.bot.say("Granted permissions for role {} to channel {}".format(role, channel))
+                await
+                asyncio.sleep(1.5)
 
     @commands.command(pass_context=True)
     async def gruppe(self, ctx, join_group=None):
         server = ctx.message.server
+
         async def send_help():
             group_channels = []
             all_channels = server.channels
@@ -379,41 +406,63 @@ Mensa:
                 description="**Verfügbare Übungsgruppen**")
             embed.add_field(name="Gruppen", value="\n".join(sorted_groups))
 
-            await self.bot.say("Gruppe nicht gefunden oder angegeben. Verfügbare Gruppen sind:")
+            await
+            self.bot.say("Gruppe nicht gefunden oder angegeben. Verfügbare Gruppen sind:")
             embed.set_footer(text='Bot by Fabi')
-            return await self.bot.say(embed=embed)
+            return await
+            self.bot.say(embed=embed)
 
         if join_group is None:
-            return await send_help()
+            return await
+            send_help()
         join_group = "übungsgruppe-{}".format(join_group)
         author = ctx.message.author
         if "übungsgruppe-" in join_group:
             try:
                 role = discord.utils.get(server.roles, name=join_group)
-                await self.bot.add_roles(author, role)
-                await self.bot.say("{}, du wurdest zu {} hinzugefügt".format(author.mention, join_group))
+                await
+                self.bot.add_roles(author, role)
+                await
+                self.bot.say("{}, du wurdest zu {} hinzugefügt".format(author.mention, join_group))
             except AttributeError:
-                await send_help()
+                await
+                send_help()
         else:
-            await send_help()
+            await
+            send_help()
 
     @commands.command(pass_context=True)
     async def gruppeverlassen(self, ctx, leave_group=None):
         server = ctx.message.server
         author = ctx.message.author
+
         async def send_help():
             all_roles = author.roles
-            await self.bot.say(all_roles)
+            role_names = []
+            embed = discord.Embed(
+                description="**Zugeordnete Übungsgruppen**")
+            for role_name in all_roles:
+                role_names.append(role_name.name)
+            embed.add_field(name="Gruppen", value="\n".join(role_names))
+            await
+            self.bot.say("Gruppe nicht gefunden oder zugeordnet. Zugeordnete Gruppen sind:")
+            embed.set_footer(text='Bot by Fabi')
+            return await
+            self.bot.say(embed=embed)
 
         if leave_group is None:
-            return await send_help()
+            return await
+            send_help()
         leave_group = "übungsgruppe-{}".format(leave_group)
         try:
             role = discord.utils.get(server.roles, name=leave_group)
-            await self.bot.remove_roles(author, role)
-            await self.bot.say("{} du wurdest aus der Gruppe {} entfernt".format(author.mention, leave_group))
+            await
+            self.bot.remove_roles(author, role)
+            await
+            self.bot.say("{} du wurdest aus der Gruppe {} entfernt".format(author.mention, leave_group))
         except AttributeError:
-            await send_help()
+            await
+            send_help()
 
 
 def setup(bot):
