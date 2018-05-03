893eed0c40764d91ee14625e7cbd3e5450c93860
fabi@ihlecloud.de <soko5113>
2018-05-03 12:24:43 +0200

    Test for separated file for uniBot

---
+++ ihlebot/uni-bot.py
@@ -0,0 +1,29 @@
+# Discord stuff
+import discord
+import asyncio
+from discord.ext import commands
+
+
+
+class UniBot:
+    """ Command definitions"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
+    def __unload(self):
+        self.session.close()
+
+
+    @commands.group(pass_context=True)
+    async def testclass(self, ctx):
+        """First Test, Commandcall"""
+        await self.bot.say('Test funktioniert')
+
+
+def setup(bot):
+    n = UniBot(bot)
+    loop = asyncio.get_event_loop()
+    bot.add_cog(n)
+    
