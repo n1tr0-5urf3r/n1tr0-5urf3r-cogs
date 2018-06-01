681d5510e6f75f0367c6433f4673bfae595a75d8
fabian.ihle <fabian.ihle@hfwu.de>
2018-06-01 08:50:58 +0200

    DM user if he wrote in wrong channel

--- ihlebot/ihlebot.py
+++ ihlebot/ihlebot.py
@@ -394,8 +394,8 @@ Mensa:
             return await self.bot.send_message(destination, embed=embed)
 
         # Harcoded channel ID :(
-        if ctx.message.channel.id is not "438732571166375938":
-            await self.bot.say("Falscher channel " + ctx.message.channel.id)
+        if ctx.message.channel.id is not 438732571166375938:
+            await self.bot.say("Falscher channel " + ctx.message.channel.id + " " + str(type(ctx.message.channel.id)))
             await send_help(ctx.message.author)
             await self.bot.send_message(ctx.message.author, "Bitte nutze den Channel #gruppenzuweisung dazu!")
 
